3dbb447fe664e19431fb8473531573a32fae21d7
David-Mains2 <dmains123@icloud.com>
2024-12-19 20:28:09 -0600

    Added Bcrypt and salt to the database

--- sql/users.sql
+++ sql/users.sql
@@ -6,12 +6,18 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL,
+    salt     VARCHAR(255) NOT NULL,
     PRIMARY KEY (username)
 );
+
+-- Generates hex value for the salt
+
+
 
 INSERT INTO users
 VALUES(
     "user",
     "pass",
     "user@example.com"
+    "A356" -- placeholder hard coded Salt
 );
