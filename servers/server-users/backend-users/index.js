cf9919aaabc1e8dc1fd466ef8d41124b0ac800cd
April G <127350089+april1703@users.noreply.github.com>
2025-01-14 23:48:38 -0600

    Update index.js

--- servers/server-users/backend-users/index.js
+++ servers/server-users/backend-users/index.js
@@ -127,7 +127,7 @@ app.post("/login", function (req, resp) {
 
 
 // TOTP verification
-app.post("/totp", (req, resp) => {
+app.post("/totp", function (req, resp) => {
   // get entered totp code
   const inputTotp = req.body;
   console.log(inputTotp);
@@ -136,6 +136,27 @@ app.post("/totp", (req, resp) => {
   return resp.status(200).json({success: true, message: "Login successful" });
 
   // get timestamp
+  /*
+  // code from class
+  const hmac = createHmac('sha256', 'secretcode');
+
+  // still need to round to 30 seconds
+  const timestamp = Math.floor(Date.now() / 1000 / 30);
+  timestamp.setSeconds(30);
+  timestamp.setMilliseconds(0);
+  console.log(timestamp);
+
+  hmac.update(timestamp.toString());
+
+  let numberPattern = /\d+/g;
+  let result = hmac.digest('hex').match(numberPattern).join('').slice(-6)
+  console.log(result);
+  if (inputTotp["totp"] === result) {
+    response.status(200).send("Code verification successful");
+  } else {
+    response status(401).send("Code comparison failed");
+  }
+  */
   const timestamp = Math.round(Date.now() / 1000 / 30);
   console.log(`timestamp: ${timestamp}`);
 
