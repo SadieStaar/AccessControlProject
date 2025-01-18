d2f0c27437a56e1493c84e52f198e2b40babdfda
ljk <94403556+ljk005@users.noreply.github.com>
2025-01-17 22:45:01 -0600

    Update common.js

--- servers/server-data/frontend-data/common.js
+++ servers/server-data/frontend-data/common.js
@@ -1,16 +1,16 @@
 var parsedUrl = new URL(window.location.href);
 
-// tologinpage: redirects user to the login page login.html
+//redirects user to the login page login.html
 function tologinpage() {
     window.location.replace("login.html");
 }
 
-// toregistrationpage: redirects user to the registration page registration.html
+//redirects user to the registration page registration.html
 function toregistrationpage() {
     window.location.replace("register.html");
 }
 
-// Function to retrieve a cookie by name
+//Function to retrieve a cookie by name
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
@@ -18,15 +18,15 @@ function getCookie(name) {
 }
 
 
-// register: take username, email, and password, send to user-management-api
+//take username, email, and password, send to user-management-api
 function register() {
-    // get values and check if password matches
+    //get values and check if password matches
     let username = document.getElementById('username').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
     let passwordcheck = document.getElementById('passwordcheck').value;
 
-    // if passwords match, send to user-management-api
+    //if passwords match, send to user-management-api
     if (password === passwordcheck) {
         console.log("Passwords match, sending data to user-management-api...");
         fetch("http://" + parsedUrl.host + ":5002/register", {
@@ -38,7 +38,7 @@ function register() {
         })
         .then((resp) => {
             console.log("returned successfully");
-            // serve the proper response
+            //serve response
             switch (resp.status) {
                 case 201:
                     // success, redirect to home page
@@ -64,19 +64,19 @@ function register() {
     }
 }
 
-// login function: take username and password, send to user-management-api
+//take username and password, send to user-management-api
 function login() {
-    // Get entered username and password
+    //Get entered username and password
     const inputusername = document.getElementById("userinput").value;
     const inputpassword = document.getElementById("password").value;
 
-    // if nothing entered, tell user to do the thing
+    //if nothing entered, tell user to do the thing
     if (!inputusername || !inputpassword) {
         alert("Please enter username and password.");
         return;
     }
 
-    // Initiate POST request to user-management-api with login credentials
+    //POST request to user-management-api with login credentials
     fetch("http://" + parsedUrl.hostname + ":5002/login", {
         method: "POST",
         headers: {
@@ -84,16 +84,16 @@ function login() {
         },
         body: JSON.stringify({ inputusername, inputpassword }),
     })
-    // handle response
+    //response
     .then(async (data) => {
         switch(data.status) {
-            case 200:  // successful login
+            case 200:  //successful 
                 console.log("Frontend redirecting to TOTP...");
                 try {
-                    const response = await data.json(); // Parse response body to get token
+                    const response = await data.json(); //parse response body to get token
                     const token = response.token;
                     if (token) {
-                        // Store the token as a cookie
+                        //store the token as a cookie
                         document.cookie = `token=${token}; Path=/; SameSite=Strict`; //HttpOnly; was here
                         console.log("JWT stored as cookie.");
                     } else {
@@ -103,24 +103,24 @@ function login() {
                     console.error("Failed to parse response body:", err);
                 }
                 window.location.replace("totp.html");
-                return; // we're done here, leave
-            case 401:  // password does not match
-            case 404:  // user not found
+                return; //we're done here, leave
+            case 401:  //password does not match
+            case 404:  //user not found
                 console.error("Username or password does not match database");
                 alert("The username or password does not match.");
                 break;
-            case 500: // database error
+            case 500: //database error
                 console.error("500: Database error: " + data.status);
                 alert("The server ran into an issue. Please try again later.");
                 break;
-            default: // something uncaught
+            default: //something uncaught
                 console.error("Unexpected response status: " + data.status);
                 alert("The server ran into an issue. Please try again later.");
         }
-        // reset page so user can try again: only on login failure
+        //reset page so user can try again: only on login failure
         location.reload();
     })
-    // error handler for fetch errors
+    //error handler 
     .catch((err) => {
         console.error(`Network or unexpected error: ${err.message}; Stack trace: ${err.stack}`);
         alert("A network error occurred. Please check your connection and try again.");
@@ -128,19 +128,19 @@ function login() {
     });
 }
 
-// totp function: take totp code, send to user-management-api
+//take totp code, send to user-management-api
 function submitTOTP() {
-    // get totp from page
+   
     const totpInput = document.getElementById('totp').value;
     console.log(`received: ${totpInput}`);
 
-    // make sure user enters something
+    //make sure user enters something
     if (!totpInput) {
         alert('Enter the 6 TOTP numbers in the textbox.');
         return;
     }
 
-    // send to the user-management-api for processing
+    //send to the api for processing
     fetch("http://" + parsedUrl.hostname + ":5002/totp", {
         method: "POST",
         headers: {
@@ -148,14 +148,14 @@ function submitTOTP() {
         },
         body: JSON.stringify({ totpInput }),
     })
-    // handle response
+    //response
     .then((resp) => {
         switch(resp.status){
-            case 200: // successful totp
+            case 200: //successful 
                 console.log("TOTP verified. Redirecting to Query page...");
                 window.location.replace("query.html");
                 return;
-            case 401: // totp doesn't match
+            case 401: //totp doesn't match
                 console.error("TOTP not verified.");
                 alert("Code not verified. Try again.");
                 break;
@@ -164,7 +164,7 @@ function submitTOTP() {
                 location.reload();
         }
     })
-    // error handler for fetch errors
+    //error handler
     .catch((err) => {
         alert("An error occurred while processing the request.");
         console.error("Unexpected error occurred:", err);
@@ -172,7 +172,7 @@ function submitTOTP() {
     });
 }
 
-// query: the original function from the forked repo
+//the original function from the forked repo
 function query() {
     const token = getCookie('token'); // Retrieve the JWT token
 
@@ -182,11 +182,11 @@ function query() {
         return;
     }
 
-    // The data API is presumably at port 80 (parsedUrl.host + "/query")
+    
     fetch("http://" + parsedUrl.hostname + "/query", {
         method: "GET",
         headers: {
-            Authorization: `Bearer ${token}`, // Include the token in the Authorization header
+            Authorization: `Bearer ${token}`, //include token in the auth header
         },
     })
     .then((resp) => {
