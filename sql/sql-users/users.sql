4323ca6d110f86a705e8da284670412aee6585f0
SadieAnn <127803877+SadieStaar@users.noreply.github.com>
2025-01-28 20:48:49 -0600

    Fixing and adding roles

--- sql/sql-users/users.sql
+++ sql/sql-users/users.sql
@@ -11,12 +11,32 @@ CREATE TABLE users (
     PRIMARY KEY (username)
 );
 
-
+-- basic user
 INSERT INTO users
 VALUES(
     "user",
     "$2b$10$31z3yI8coig/3nE8ae16UOWir18T2b3VC.PiAx9zPcuM8mkj6sjaa",
     "member",
+    "user@example.com",
+    "abc1"
+);
+
+-- premium user
+INSERT INTO users
+VALUES(
+    "premium",
+    "$2b$10$31z3yI8coig/3nE8ae16UOWir18T2b3VC.PiAx9zPcuM8mkj6sjaa",
+    "premium",
+    "premium@example.com",
+    "abc1"
+);
+
+-- admin
+INSERT INTO users
+VALUES(
+    "admin",
+    "$2b$10$31z3yI8coig/3nE8ae16UOWir18T2b3VC.PiAx9zPcuM8mkj6sjaa",
+    "admin",
     "user@example.com",
     "abc1"
 );
